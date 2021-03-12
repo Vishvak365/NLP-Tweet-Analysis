af39d46504002441804a90cf6b47c05f3c9a7ecb
Vishvak Seenichamy <vishvak365@gmail.com>
2021-03-12 14:20:50 -0500

    classifier testing

--- JsonToPD.py
+++ JsonToPD.py
@@ -1,19 +1,26 @@
 import json
 import pandas as pd
 import os
-reviews = []
-rating = []
-for filename in os.listdir('data'):
-    if filename.endswith(".json"):
-        print(filename)
-        with open('data/'+filename) as f:
-            data = json.load(f)
-            for x in data:
-                reviews.append(x['reviewText'])
-                rating.append(x['overall'])
-data = {'reviews': reviews, 'rating': rating}
-df = pd.DataFrame(data, columns=['reviews', 'rating'])
-df.to_pickle('amazon_review_data.pd')
+import nltk
+from nltk.tokenize import word_tokenize
+# reviews = []
+# rating = []
+# for filename in os.listdir('data'):
+#     if filename.endswith(".json"):
+#         print(filename)
+#         with open('data/'+filename) as f:
+#             data = json.load(f)
+#             for x in data:
+#                 reviews.append(x['reviewText'])
+#                 rating.append(x['overall'])
+# data = {'reviews': reviews, 'rating': rating}
+# df = pd.DataFrame(data, columns=['reviews', 'rating'])
+# df.to_pickle('amazon_review_data.pd')
 # df.to_csv('amazon_video.csv')
-# df = pd.read_pickle('amazon_instant_video')
+df = pd.read_pickle('amazon_review_data.pd')
+# df['reviews'][0] = "asdf"
 # print(df['reviews'])
+tokens = []
+for x in df['reviews']:
+    tokens.append(word_tokenize(x))
+# print(tokens)
